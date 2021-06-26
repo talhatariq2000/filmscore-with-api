b8a74307edbf561ce9c7401668d10d720b118f84
talhatariq2000 <76400023+talhatariq2000@users.noreply.github.com>
2021-06-26 13:54:45 +0500

    Update actorsajax.js

--- codes/actorsajax.js
+++ codes/actorsajax.js
@@ -5,7 +5,7 @@ $(function(){
 
 function loadactorlist() {
     $.ajax({
-        url:"http://localhost:3000/api/films/",
+        url:"http://localhost:3000/api/actors/",
         method:"GET",
         success:function(response){
             console.log(response);
@@ -13,12 +13,12 @@ function loadactorlist() {
             actorlist.empty();
             for(var i=0;i<response.length;i++)
             {
-                if(response[i].type==="film")
+                if(response[i].type==="actor")
                 {
                     var actor = response[i];
                     actorlist.append(`
                         <div id="${actor._id}" class="actorlistcontainer">
-                            <h5>${actor.cast[1]}</h5>
+                            <h5>${actor.name}</h5>
                         </div>
                     `);
                 }
